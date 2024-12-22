16f11e2ae9354554d61adff4adff1c8565cadc5b
Mohsen Alizadeh <mohsen@alizadeh.us>
2024-12-22 10:11:43 -0800

    bundle gem valkey --no-exe --ext=rust --test=minitest --linter=rubocop

---
+++ ruby/lib/valkey.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "valkey/version"
+require_relative "valkey/valkey"
+
+module Valkey
+  class Error < StandardError; end
+  # Your code goes here...
+end
